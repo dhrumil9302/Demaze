13b1eb73b18230a5b71a78e6d29344af921238cf
dhrumil9302 <suthardhrumil5@gmail.com>
2023-12-14 11:41:57 +0530

    Updated

--- Components/Service.js
+++ Components/Service.js
@@ -12,14 +12,81 @@ const Service = () =>{
             <div>
                 <h5 class="par">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the <br/>industry's standard dummy text ever since the </h5> 
             </div>
-            <div class="box">
-                <div class="b1">
-
-                </div>
-
-            </div>
-        </div>
         
+            <div class="Row">
+                <div class="card">
+                <div class="container">
+                    <h4><b>Seo</b></h4> 
+                        <p>Optimizing your website to rank higher...</p> 
+                </div>
+            </div>
+            <div class="card">
+                <div class="container">
+                    <h4><b>Email Marketing</b></h4> 
+                        <p>Sending personalized emails to....</p> 
+                </div>
+            </div>
+            <div class="card">
+                <div class="container">
+                    <h4><b>Social Medial Marketing</b></h4> 
+                        <p>Promoting your brand, products</p> 
+                </div>
+            </div>
+            <div class="card">
+                <div class="container">
+                    <h4><b>PPC</b></h4> 
+                        <p>Running targeted ads on search engines...</p> 
+                </div>
+            </div>
+            </div> 
+        </div>
+
+
+
+
+
+
+        <div id='Service' class="log">
+            <div> 
+                <h4 class="ser1">Our Specialists Worker</h4>
+            </div>
+            <div>
+                <h2 class="par1">Meet Our expert team worksor  <br/>your business</h2> 
+            </div>
+        
+            <div class="Row">
+                <div class="card">
+                <img src="./image/perosn.jpg" ></img>
+                <div class="container">
+                    <h4><b>Noach Mehil</b></h4> 
+                        <p>CEO of company</p> 
+                </div>
+            </div>
+            <div class="card">
+                <img src="person.png" ></img>
+                <div class="container">
+                    <h4><b>Mattihu Dabit</b></h4> 
+                        <p>Marketing manager</p> 
+                </div>
+            </div>
+            <div class="card">
+                <img src="img_avatar.png"></img>
+                <div class="container">
+                    <h4><b>Olevia Jaiden</b></h4> 
+                        <p>Project manager</p> 
+                </div>
+            </div>
+            <div class="card">
+                <img src="img_avatar.png"></img>
+                <div class="container">
+                    <h4><b>Emma Wailiam</b></h4> 
+                        <p>Product designer</p> 
+                </div>
+            </div> 
+            </div> 
+        </div>
+    
+     
         </>
     )
 }
